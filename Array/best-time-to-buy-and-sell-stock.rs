ef59ffef3d19b68f6c80c92af0ba7c540a8c82ed
ismaj <ismaj@protonmail.com>
2021-08-15 13:53:02 +0000

    faster stock

--- Array/best-time-to-buy-and-sell-stock.rs
+++ Array/best-time-to-buy-and-sell-stock.rs
@@ -1,12 +1,18 @@
-use std::cmp::{min, max};
+use std::cmp::{max, Ordering};
 
 impl Solution {
     pub fn max_profit(prices: Vec<i32>) -> i32 {
+        if prices.len() == 0 { return 0 }
+
         let mut res: i32 = 0;
-        let mut cheapest: i32 = std::i32::MAX;
-        prices.iter().for_each(|&price| {
-            cheapest = min(cheapest, price);
-            res = max(res, price-cheapest);
+        let mut cheapest: i32 = prices[0];
+        
+        prices.iter().for_each(|price| {
+            match cheapest.cmp(price){
+                Ordering::Less => res = max(res, price-cheapest),
+                Ordering::Greater => cheapest = *price,
+                Ordering::Equal => (),
+            }
         });
         res
     }
