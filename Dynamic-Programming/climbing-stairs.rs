68dc397e0924ddadd4727f21808f947db3612af3
ismaj <ismaj@protonmail.com>
2021-11-07 20:22:31 +0000

    constant space

--- Dynamic-Programming/climbing-stairs.rs
+++ Dynamic-Programming/climbing-stairs.rs
@@ -3,13 +3,15 @@
 struct Solution;
 
 impl Solution {
+    // O(n) time - O(1) space - dynamic programming
     pub fn climb_stairs(n: i32) -> i32 {
-        let mut dp: Vec<i32> = vec![0; n as usize + 2];
-        dp[0] = 0;
-        dp[1] = 1;
-        for i in 2..=(n+1) {
-            dp[i as usize] = dp[(i-1) as usize] + dp[(i-2) as usize];
-        }
-        dp[n as usize + 1]
+        let mut previous = 0;
+        let mut current = 1;
+        (0..n).for_each(|_| {
+            let next = current + previous;
+            previous = current;
+            current = next;
+        });
+        current
     }
 }
